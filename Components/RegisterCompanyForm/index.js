4265dc8578f93ca0405039c08ebcf09a03871bd8
AxelMatus <axelantonio@gmail.com>
2022-05-21 02:13:47 -0600

    RegisterCompanyForm Modularized and changes in the RegisterCompanyForm.css

--- Components/RegisterCompanyForm/index.js
+++ Components/RegisterCompanyForm/index.js
@@ -1,6 +1,22 @@
 import Button from '../ButtonMedium'
 import Styles from '../../styles/RegisterCompanyForm.module.css';
 
+function RegisterCompanyInput(props){
+    const required = props.required
+
+    return(
+        
+        <div className={Styles.InputElement}>
+            <label className={Styles.InputLabel} forhtml={props.name}>  {props.tag} </label>
+            {
+            props.required ?
+                <input className={Styles.InputTextBox} type={props.type} id={props.name} required /> :
+                <input className={Styles.InputTextBox} type={props.type} id={props.name} />
+            }
+        </div>
+    );
+}
+
 function RegisterCompanyForm (){
     return(
         <>
@@ -16,52 +32,30 @@ function RegisterCompanyForm (){
             <p className={Styles.subtitle}> Ingresa los datos de tu Empresa </p>
                 <form>
                     <p className={Styles.Formsubtitle}> Datos de Empresa</p>
-                    <div className={Styles.SubEntry}>
-                        <div className={Styles.InputElement}>
-                            <label className={Styles.InputLabel} forhtml="name">  Nombre: </label>
-                            <input className={Styles.InputTextBox} type="text" id="name" />
-                        </div>
 
-                        <div className={Styles.InputElement}>
-                            <label className={Styles.InputLabel} forhtml="BusinessName"> Razón Social: </label>
-                            <input className={Styles.InputTextBox} type="text" id="BusinessName"  />
-                        </div>
+                    <div className={Styles.SubEntry}>
+                        <RegisterCompanyInput name="Name" type="text" tag="Nombre: *" required={true} />
+                        <RegisterCompanyInput name="BusinessName" type="text" tag="Razón Social: *" required={true} />
                     </div>
 
                     <div className={Styles.SubEntry}>
-                        <div className={Styles.InputElement}>
-                            <label className={Styles.InputLabel} forhtml="LegalId"> Cédula Jurídica: </label>
-                            <input className={Styles.InputTextBox} type="text" id="LegalId"/>
-                        </div>
+                        <RegisterCompanyInput name="LegalId" type="text" tag="Cédula Jurídica: *" required={true} />
                     </div>
 
                     <p className={Styles.Formsubtitle}> Contacto de Empresa </p>
-                    <div className={Styles.SubEntry}>
-                        <div className={Styles.InputElement}>
-                            <label className={Styles.InputLabel} forhtml="PhysicalAddress"> Dirección Física: </label>
-                            <input className={Styles.InputTextBox} type="text" id="PhysicalAddress" />
-                        </div>
 
-                        <div className={Styles.InputElement}>
-                            <label className={Styles.InputLabel} htmfor="Email"> Correo Electrónico: </label>
-                            <input className={Styles.InputTextBox} type="text" id="Email" />
-                        </div>
+                    <div className={Styles.SubEntry}>
+                        <RegisterCompanyInput name="PhysicalAddress" type="text" tag="Dirección Física: *" required={true} />
+                        <RegisterCompanyInput name="Email" type="text" tag="Correo Electrónico: *" required={true} />
                     </div>
 
                     <div className={Styles.SubEntry}>
-                        <div className={Styles.InputElement} >
-                            <label className={Styles.InputLabel} forhtml="Phone"> Teléfono: </label>
-                            <input className={Styles.InputTextBox} type="text" id="Phone" />
-                        </div>
-
-                        <div className={Styles.InputElement}>
-                            <label className={Styles.InputLabel} forhtml="Website"> Página Web: </label>
-                            <input className={Styles.InputTextBox} type="text" id="Website" />
-                        </div>
+                        <RegisterCompanyInput name="Phone" type="text" tag="Teléfono: *" required={true} />
+                        <RegisterCompanyInput name="Website" type="text" tag="Página Web:" required={false} />
                     </div>
-                    
-                    <Button ButtonText="Enviar "> </Button>
-                    
+                    <div className={Styles.ButtonPosition}>
+                        <Button ButtonText="Enviar "> </Button>
+                    </div>
                 </form>
             </div>
         </>
