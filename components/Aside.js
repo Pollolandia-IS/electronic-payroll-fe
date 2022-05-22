ee798fdf7d1a7c9811185d5c6692bc12ba24ae5e
David Atias <davidaar2000@gmail.com>
2022-05-22 15:00:58 -0600

    Added Aside component

--- components/Aside.js
+++ components/Aside.js
@@ -1,57 +1,42 @@
-import React from 'react';
-import { BsCardText, BsGearFill, BsArrowCounterclockwise, BsPersonCircle, BsFillArrowRightSquareFill } from 'react-icons/bs';
-import styles from '../../../styles/Sidebar.module.css';
+import AsideItem from "./AsideItem"
+import styles from "../styles/Aside.module.css"
 
+const Aside = (props) => {
+    const createItems = () => {
+        return props.items.map((item, index) => {
+            return <AsideItem key={index} item={item} />
+        })
+    }
 
-
-// const SidebarItem = [
-//     {
-//         title: 'Nomina',
-//         icon: <BsCardText/>,
-//         link: '/'
-//     },
-//     {
-//         title: 'Ajustes',
-//         icon: <BsGearFill />,
-//         link: '/'
-//     },
-//     {
-//         title: 'Historial',
-//         icon: <BsArrowCounterclockwise />,
-//         link: '/'
-//     },
-//     {
-//       title: 'Perfil',
-//       icon: <BsPersonCircle />,
-//       link: '/'
-//   },
-//   {
-//     title: 'Cerrar Session',
-//     icon: <BsFillArrowRightSquareFill />,
-//     link: '/'
-// },
-// ]
-
-const Sidebar = (props) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.sidebar}>
-        <div className={styles.sidebarBody}>
-          <ul className={styles.sidebarBodyList}>
-            {props.SidebarItem.map(item => (
-              <li key={item.title} className={styles.sidebarBodyListItem}>
-                <a className={styles.sidebarBodyListItem} href={item.link}>
-                  {item.icon}
-                  <span className={styles.objectSidebar}>{item.title}</span>
-                </a>
-              </li>
-            ))}
+    return (
+        <aside className={styles.aside}>
+          <ul className={styles.aside__list}>
+            {createItems()}
           </ul>
-        </div>
-      </div>
-    </div>
-
-  );
+        </aside>
+    )
 }
+
+// const Sidebar = (props) => {
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.sidebar}>
+//         <div className={styles.sidebarBody}>
+//           <ul className={styles.sidebarBodyList}>
+//             {props.SidebarItem.map(item => (
+//               <li key={item.title} className={styles.sidebarBodyListItem}>
+//                 <a className={styles.sidebarBodyListItem} href={item.link}>
+//                   {item.icon}
+//                   <span className={styles.objectSidebar}>{item.title}</span>
+//                 </a>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+
+//   );
+// }
     
-export default Sidebar;
+export default Aside;
