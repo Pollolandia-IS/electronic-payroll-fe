d1dbbc5a2c780bcd2d88b666712df8b961e69583
AndreyEliVar <andrey.elizondovargas@ucr.ac.cr>
2022-05-21 22:06:41 -0600

    Remove empty pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -11,22 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <NavBar navItems={[["Inciio",true,"/"],["Proyecto",false,"/Proyectos"]]} />
     </div>
   )
 }
