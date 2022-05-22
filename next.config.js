b46b2aeb9b16f37eab0da590a3071287f0216448
David Atias <davidaar2000@gmail.com>
2022-05-22 16:09:05 -0600

    Added position fixed to navbar and aside

--- next.config.js
+++ next.config.js
@@ -8,17 +8,4 @@ const path = require('path');
 module.exports = {
   experimental: { images: { layoutRaw: true } },
   nextConfig,
-  webpack(config) {
-    config.resolve.alias['~'] = path.resolve(__dirname);
-    config.module.rules.push({
-      test: /\.(woff|ttf|svg)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000,
-        },
-      },
-    });
-    return config;
-  },
 };
