033b5bb598afd58c2e2a7bec85861b5003f7a101
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-05-22 16:42:52 -0600

    Added NavBar component to view

--- pages/CuentaAdmin/index.js
+++ pages/CuentaAdmin/index.js
@@ -1,31 +1,30 @@
 import Image from 'next/image';
 import Form from '../../components/Form.js';
+import NavBar from '../../components/NavBar.js';
 import styles from '../../styles/CuentaAdmin.module.css';
 
+
+
 function CuentaAdmin() {
-    //TODO: add NavBar (the component)
-    //TODO: add links for "Inicio" and "Sobre Nosotros" 
     //TODO: add info in Footer
     //TODO: fix gradient of the footer
+    const navItems = [
+        ["Inicio", false, "/"],
+        ["Sobre Nosotros", false, "/"]
+    ];
     return (
         <>
-            <header>
-                <nav className={styles.CA__nav}>
-                    <ul className={styles.CA__nav__ul}>
-                        <li><img className={styles.CA__nav__li} src='Logo_Icon.svg'/></li>
-                        <li className={styles.CA__nav__li}><a href='#'>Inicio</a></li>
-                        <li className={styles.CA__nav__li}><a href='#'>Sobre Nosotros</a></li>
-                    </ul>
-                </nav>
+            <header className={styles.CA__header}>
+                <NavBar navItems={navItems}/>
                 <h1 className={styles.CA__h1}>Ingresa tus datos:</h1>
                 <h2 className={styles.CA__h2}>Los campos con * son obligatorios.</h2>
             </header>
             <div>
-                <Image
+                <Image className={styles.CA__BackG__img}
                     alt="Background"
                     src="/Cuenta_Admin_Background.svg"
                     layout="fill"
-                    objectFit="cover"
+                    objectFit="contain"
                     quality={100}
                 />
                 <img className={styles.CA__img} src='Logo.svg'/>
