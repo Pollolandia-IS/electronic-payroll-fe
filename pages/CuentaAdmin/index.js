8da193632d68f948e969aad25490d037f683ba40
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-05-22 18:14:33 -0600

    Added styling and info in the footer element along with icons for it

--- pages/CuentaAdmin/index.js
+++ pages/CuentaAdmin/index.js
@@ -3,37 +3,37 @@ import Form from '../../components/Form.js';
 import NavBar from '../../components/NavBar.js';
 import styles from '../../styles/CuentaAdmin.module.css';
 
-
-
 function CuentaAdmin() {
-    //TODO: add info in Footer
-    //TODO: fix gradient of the footer
     const navItems = [
         ["Inicio", false, "/"],
         ["Sobre Nosotros", false, "/"]
     ];
+
     return (
         <>
-            <header className={styles.CA__header}>
-                <NavBar navItems={navItems}/>
+            <NavBar navItems={navItems}/>
+            <div>
                 <h1 className={styles.CA__h1}>Ingresa tus datos:</h1>
                 <h2 className={styles.CA__h2}>Los campos con * son obligatorios.</h2>
-            </header>
+            </div>
             <div>
-                <Image className={styles.CA__BackG__img}
+                <Image className={styles.CA__backG__img}
                     alt="Background"
                     src="/Cuenta_Admin_Background.svg"
                     layout="fill"
-                    objectFit="contain"
+                    objectFit="cover"
                     quality={100}
                 />
                 <img className={styles.CA__img} src='Logo.svg'/>
             </div>
             <Form titles= {[["Cedula", "text", true], ["Email", "email", true], ["Teléfono", "tel", true], 
                 ["Nombre", "text", true], ["Contraseña", "password", true], ["Confirmar Contraseña", "password", 
-                    true]]} button = "CONFIRMAR"/>
+                    true]]} button = "Confirmar"/>
             <footer className={styles.CA__footer}>
-
+                <p className={styles.CA__footer__p}>
+                    <span className={`icon PL-telf ${styles.CA__footer__span}`}></span>Teléfono: (+506) 1234 - 5678<br/>
+                    <span className={`icon PL-mail ${styles.CA__footer__span}`}></span>Dirección electrónica: PLSolutions@PLS.co.cr<br/>
+                </p>
             </footer>
         </>
     );
