2f5a547daefb64cf9bc264ffd6292b41f28476d8
derek.suarez <derek.suarez@ucr.ac.cr>
2022-05-24 22:37:38 -0600

    Changes on RegisterForm according to ticket review

--- Components/RegisterForm.js
+++ Components/RegisterForm.js
@@ -1,20 +1,16 @@
 import { useState, useEffect } from "react";
 import styles from '../styles/RegisterForm.module.css';
-
+import NavForm from './NavForm.js';
 const RegisterForm = (props) => {
     return (
         <>
-            <form className={styles.RegisterForm}>
                 {props.titles.map(([title, type, isRequired]) => (
                     <>
                         <input className={styles.Form__input} type={type} name={title} placeholder={title} required={isRequired} />
                         <hr className={styles.Form__border} />
                     </>
                 ))}
-            </form>
         </>
     );
 }
-
-
 export default RegisterForm;
