fca05d264e90ec83931dc00417bc12de2862a3e8
derek.suarez <derek.suarez@ucr.ac.cr>
2022-05-25 14:36:44 -0600

    RegisterForm component ready

--- Components/RegisterForm.js
+++ Components/RegisterForm.js
@@ -4,11 +4,20 @@ const RegisterForm = (props) => {
     return (
         <>
             <form className={styles.RegisterForm}>
-                {props.titles.map(([title, type, isRequired]) => (
-                    <>
-                        <input className={styles.Form__input} type={type} name={title} placeholder={title} required={isRequired} />
-                        <hr className={styles.Form__border} />
-                    </>
+                {props.titles.map(([title, type, isRequired, isFile]) => (
+                    <div>
+                        {isFile ? (
+                            <div>
+                                <input id="crotolamo" className={styles.Form__input__file} type={type} name={title} placeholder={title} required={isRequired} title= "" />
+                                <label className={styles.Form__label__file} placeholder={title} for="crotolamo">Foto</label>
+                                <hr className={styles.Form__border} />
+                            </div>
+                        ) : (
+                            <div>
+                                <input className={styles.Form__input} type={type} name={title} placeholder={title} required={isRequired} />
+                                <hr className={styles.Form__border} />
+                            </div>)}
+                    </div>
                 ))}
                 <button className={styles.Form__button} type="Submit">{props.button}</button>
             </form>
