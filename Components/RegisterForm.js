e8f10f1f21d5e14eee73e545e8ce702c17b6b0d7
derek.suarez <derek.suarez@ucr.ac.cr>
2022-05-24 23:58:22 -0600

    File input format and modal pending

--- Components/RegisterForm.js
+++ Components/RegisterForm.js
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
 import styles from '../styles/RegisterForm.module.css';
-import NavForm from './NavForm.js';
 const RegisterForm = (props) => {
     return (
         <>
+            <form className={styles.RegisterForm}>
                 {props.titles.map(([title, type, isRequired]) => (
                     <>
                         <input className={styles.Form__input} type={type} name={title} placeholder={title} required={isRequired} />
                         <hr className={styles.Form__border} />
                     </>
                 ))}
+                <button className={styles.Form__button} type="Submit">{props.button}</button>
+            </form>
         </>
     );
 }
