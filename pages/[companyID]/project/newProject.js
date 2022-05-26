22134f01ba2e0997bfc55850ad6dc922d8650a2c
David Atias <davidaar2000@gmail.com>
2022-05-26 06:34:14 -0600

    Added views and database communication

--- pages/[companyID]/project/newProject.js
+++ pages/[companyID]/project/newProject.js
@@ -1,7 +1,10 @@
+import Modal from '/components/Modal';
 import Aside from "/components/Aside";
 import NavBar from "/components/NavBar";
 import ProjectCard from "/components/projectCard";
-import styles from "/styles/Projects.module.css";
+import styles from "/styles/newProject.module.css";
+import Router from "next/router";
+import { useState } from "react";
 
 export async function getServerSideProps(context) {
   const { companyID} = context.params;
@@ -12,7 +15,32 @@ export async function getServerSideProps(context) {
 };
 }
 
-const Projects = ({companyID}) => {
+const newProject = ({companyID}) => {
+
+  const submitInfo = async (event) => {
+    event.preventDefault();
+    const info = {
+        companyID: companyID,
+        name: event.target.name.value,
+        currency: event.target.currency.value,
+        amountBenefit: event.target.amountBenefit.value,
+        moneyBenefit: event.target.moneyBenefit.value,
+        frequency: event.target.frequency.value,
+    };
+    try {
+      await fetch ('/api/project', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(info),
+      });
+      await Router.push(`/${companyID}/project`);
+  } catch (error) {
+      console.error(error);
+  }
+  };
+
+  const [modalOpened, setModalOpened] = useState(true);
+
   const navItems = [
     ["Proyectos", true, "/RegisterCompany"],
     ["Reportes", false, "/RegisterCompany"],
@@ -26,7 +54,7 @@ const Projects = ({companyID}) => {
         name: 'Nómina',
         icon: 'payroll',
         dropDown: [
-          ['+ Crear Proyecto', `/${companyID}/project/newProject`],
+          ['+ Crear Proyecto', ''],
         ],
       },
       {
@@ -120,8 +148,18 @@ const Projects = ({companyID}) => {
           startDate="01/01/2020 - 01/08/2022"
         />
     </main>
+    <Modal title="Nuevo Proyecto" modalOpened={modalOpened} setModalOpened={setModalOpened}>
+    <form className={styles.modal} onSubmit={submitInfo} >
+      <input className={styles.modal__input} type="text" name='name' placeholder="Nombre del Proyecto" />
+      <input className={styles.modal__input} type="text" name='currency' placeholder="Moneda" />
+      <input className={styles.modal__input} type="text" name='amountBenefit' placeholder="Beneficios Maximos" />
+      <input className={styles.modal__input} type="text" name='moneyBenefit' placeholder="Monto Max Beneficios" />
+      <input className={styles.modal__input} type="text" name='frequency' placeholder="Frecuencia de Pago" />
+      <input className={styles.modal__submit} type="submit" value="Crear" />
+    </form>
+    </Modal>
     </>
   );
 }
 
-export default Projects;
+export default newProject;
