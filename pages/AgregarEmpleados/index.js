992fef85aa4907ba1b0b58fccad26b0e0434b069
derek.suarez <derek.suarez@ucr.ac.cr>
2022-05-25 21:38:46 -0600

    NavBar and Aside implemented

--- pages/AgregarEmpleados/index.js
+++ pages/AgregarEmpleados/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import RegisterForm from '../../components/RegisterForm.js';
 import Modal from '../../Components/Modal.js'
 import NavBar from '../../Components/NavBar.js';
+import Aside from '../../Components/Aside.js';
 import styles from '../../styles/AgregarEmpleados.module.css';
 
 function AgregarEmpleados(){
@@ -10,22 +11,38 @@ function AgregarEmpleados(){
     const openModal = () => {
         setModalOpened(true);
     }
+    const AsideItems = [
+        {
+          name: 'Agregar Empleado',
+          icon: 'profile',
+          dropDown: [['- Nuevo Empleado', '/registerEmployeeModal'],],
+        },
+        {
+          name: 'Perfil',
+          icon: 'profile',
+          dropDown: [],
+        },
+        {
+          name: 'Cerrar Sesión',
+          icon: 'logout',
+          dropDown: [],
+        },
+      ];
+      
+      const navItems = [
+        ["Inicio", false, "/"],
+        ["Proyectos", false, "/projects"],
+        ["Reportes", false, "/reports"],
+        ["Empleados", true, "/employees"],
+        ["Deducciones", false, "/deductions"],
+        ["Beneficios", false, "/benefits"],
+      ];
+    
     return(
         /*<button className={styles.Form__button} type="Submit"><span className="icon PL-arrow"></span> {props.button}</button>*/
         <>
-            <header>
-                <nav className={styles.AE__nav}>
-                    <ul className={styles.AE__nav__ul}>
-                        <li><img className={styles.AE__nav__li} src='Logo_Icon.svg'/></li>
-                        <li className={styles.AE__nav__li}><a href='#'>Inicio</a></li>
-                        <li className={styles.AE__nav__li}><a href='#'>Proyectos</a></li>
-                        <li className={styles.AE__nav__li}><a href='#'>Reportes</a></li>
-                        <li className={styles.AE__nav__li}><a href='#'>Empleados</a></li>
-                        <li className={styles.AE__nav__li}><a href='#'>Deducciones</a></li>
-                        <li className={styles.AE__nav__li}><a href='#'>Beneficios</a></li>
-                    </ul>
-                </nav>
-            </header>
+            <NavBar navItems={navItems}></NavBar>
+            <Aside items={AsideItems}></Aside>
             <button className={styles.button} onClick={openModal}> Agregar Empleado</button>
             <Modal title="Agregar empleado" modalOpened={modalOpened} setModalOpened ={setModalOpened}>
                 <RegisterForm titles= {[["Nombre del empleado", "text", true], ["Cédula", "text", true], ["Email", "email", true], 
