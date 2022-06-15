fb73084f56d8a738085485e63d1e42e02223fa0c
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-06-15 00:31:45 -0600

    Created api route to get the acquired benefits by the employee

---
+++ pages/api/acquiredBenefits.js
@@ -0,0 +1,23 @@
+import {prisma} from '/.db';
+
+export default function handler (req, res) {
+    if (req.method == "POST") {
+        getAcquiredBenefits(req, res);
+    };
+}
+
+async function getAcquiredBenefits (req, res) {
+    try {
+        const selectedBenefits = await prisma.selecciona.findMany({
+            where: {
+                cedulaEmpleado: req.body.employeeID,
+                cedulaJuridica: req.body.companyID,
+                nombreProyecto: req.body.projectName,
+            }
+        });
+        res.status(200).json(selectedBenefits);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
