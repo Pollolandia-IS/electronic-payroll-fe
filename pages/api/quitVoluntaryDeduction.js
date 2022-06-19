f0ba39501ccb8202e58326bb9832008ad889f3a4
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-06-19 16:31:44 -0600

    Created api route to remove escoge relation from the DB

---
+++ pages/api/quitVoluntaryDeduction.js
@@ -0,0 +1,26 @@
+import {prisma} from '/.db';
+
+export default function handler (req, res) {
+    if (req.method == "POST") {
+        deleteVoluntaryDeduction(req, res);
+    };
+}
+
+async function deleteVoluntaryDeduction (req, res) {
+    try {
+        const result = await prisma.escoge.delete({
+            where: {
+                cedulaEmpleado_cedulaJuridica_nombreProyecto_nombreDeduccion: {
+                    cedulaEmpleado: req.body.employeeID,
+                    cedulaJuridica: req.body.companyID,
+                    nombreProyecto: req.body.projectName,
+                    nombreDeduccion: req.body.deductionName,
+                }
+            },
+        });
+        res.status(200).json({ success: result });
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
