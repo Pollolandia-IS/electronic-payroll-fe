94088fbedf310ca8132e3548727fb79dd01de248
AndreyEliVar <andrey.elizondovargas@ucr.ac.cr>
2022-06-19 10:52:24 -0600

    Employee hour fix

---
+++ components/HourTable.js
@@ -0,0 +1,37 @@
+import DataTable from "./DataTable";
+
+export default function HourTable(props) {
+  const columns = [
+    {
+      field: "hours",
+      headerName: "Horas",
+      width: 100,
+      align: "center",
+      headerAlign: "center",
+      disableColumnMenu: true,
+    },
+    {
+      field: "date",
+      headerName: "Fecha de Registro",
+      width: 200,
+      align: "center",
+      headerAlign: "center",
+      disableColumnMenu: true,
+    },
+    {
+      field: "state",
+      headerName: "Estado",
+      width: 200,
+      align: "center",
+      headerAlign: "center",
+      disableColumnMenu: true,
+    },
+  ];
+
+  //TODO - Get the rows from the database
+  //TODO - Remove sort or fix date sorting (add sortable: false to the columns)
+
+  return (
+    <DataTable rows={props.rows} columns={columns} height={797} width={502} />
+  );
+}
