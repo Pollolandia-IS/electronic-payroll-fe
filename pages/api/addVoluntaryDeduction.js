0dc98b29d2439b71cccabf73a7a33e7e7316ec1d
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-06-19 16:28:22 -0600

    Created api route to get the Voluntary deductions from the DB

---
+++ pages/api/addVoluntaryDeduction.js
@@ -0,0 +1,25 @@
+import {prisma} from '/.db';
+
+export default function handler (req, res) {
+    if (req.method == "POST") {
+        selectDeduction(req, res);
+    };
+}
+
+async function selectDeduction (req, res) {
+    try {
+        const result = await prisma.escoge.create({
+            data: {
+                cedulaEmpleado: req.body.employeeID,
+                cedulaJuridica: req.body.companyID,
+                nombreProyecto: req.body.projectName,
+                nombreDeduccion: req.body.deductionName,
+                aporte: req.body.amount,
+            }
+        });
+        res.status(200).json({ success: result });
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
