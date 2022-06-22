6923bbb8477c27634d0e0eb5179980bd2e7698f5
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-06-22 03:50:03 -0600

    Added functionality for the employer to delete projects

--- pages/[companyID]/project/index.js
+++ pages/[companyID]/project/index.js
@@ -6,88 +6,128 @@ import styles from "/styles/Projects.module.css";
 import { useState } from "react";
 import Search from "../../../components/Search";
 import IconBox from "../../../components/IconBox";
-import AddIcon from '@mui/icons-material/Add';
-import {prisma} from "/.db";
-import safeJsonStringify from 'safe-json-stringify';
+import AddIcon from "@mui/icons-material/Add";
+import { prisma } from "/.db";
+import safeJsonStringify from "safe-json-stringify";
 import NewProjectModal from "../../../components/NewProjectModal";
+import DeleteModal from "../../../components/DeleteModal";
 
 export async function getServerSideProps(context) {
-  const { companyID } = context.params;
+    const { companyID } = context.params;
 
-  let projects = await prisma.proyecto.findMany({
-    where: {
-      cedulaJuridica: companyID
-    },
-    include: {
-      _count: {
-        select: {
-          esContratado: true,
+    let projects = await prisma.proyecto.findMany({
+        where: {
+            cedulaJuridica: companyID,
         },
-      },
-    },
-  });
-  const projectsString = JSON.parse(safeJsonStringify(projects));
-
-  return {
-    props: {
-      companyID,
-      projectsString,
-    },
-  };
-}
-
-
-
-const Projects = ({companyID, projectsString, contracts,employees}) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const convertDate = (date) => {
-    let dateArray = date.split("-");
-    dateArray[2] = dateArray[2].split("T")[0];
-    let newDate = dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
-    return newDate;
-  }
-
-  const getProjects = () => {
-    return projectsString.map(project => {
-      return <ProjectCard key={project.nombre} projects={projectsString} companyID={project.cedulaJuridica} name={project.nombre}
-        employeeCount={project._count.esContratado} maxBen={project.cantidadMaximaBeneficios}
-        maxAmountBen={project.montoMaximoBeneficio} currency={project.moneda}
-        frequency={project.frecuenciaPago} date={convertDate(project.fechaInicio)} />
+        include: {
+            _count: {
+                select: {
+                    esContratado: true,
+                },
+            },
+        },
     });
-  }
-  
-  const getRows = () => {
-    let rows = [];
-    const projects = getProjects();
-    for (let i = 0; i < projects.length; i += 2) {
-      rows.push(
-        <div key={i} className={styles.main__row}>
-          {projects[i]}
-          {projects[i + 1]}
-        </div>
-      );
-    }
-    return rows;
-  }
-  
+    const projectsString = JSON.parse(safeJsonStringify(projects));
 
-
-  return (
-    <>
-    <NewProjectModal isOpen={isOpen} setIsOpen={setIsOpen} companyID={companyID}/>
-    <Sidebar selected={2} username="David Atias" />
-    <main className={styles.main}>
-      <div className={styles.main__header}>
-        <Search placeholder="Buscar proyecto..."/>
-        <IconBox action={() => setIsOpen(true)} ><AddIcon fontSize="large" /></IconBox>
-      </div>
-      <div className={styles.main__content}>
-        {getRows()}
-      </div>
-    </main>
-    </>
-  );
+    return {
+        props: {
+            companyID,
+            projectsString,
+        },
+    };
 }
+
+const Projects = ({ companyID, projectsString, contracts, employees }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isOpenRemove, setIsOpenRemove] = useState(false);
+    const [selectedProject, setSelectedProject] = useState("");
+
+    const convertDate = (date) => {
+        let dateArray = date.split("-");
+        dateArray[2] = dateArray[2].split("T")[0];
+        let newDate = dateArray[2] + "/" + dateArray[1] + "/" + dateArray[0];
+        return newDate;
+    };
+
+    const getProjects = () => {
+        return projectsString.map((project) => {
+            return (
+                <ProjectCard
+                    key={project.nombre}
+                    projects={projectsString}
+                    companyID={project.cedulaJuridica}
+                    name={project.nombre}
+                    employeeCount={project._count.esContratado}
+                    maxBen={project.cantidadMaximaBeneficios}
+                    maxAmountBen={project.montoMaximoBeneficio}
+                    currency={project.moneda}
+                    frequency={project.frecuenciaPago}
+                    date={convertDate(project.fechaInicio)}
+                    setIsOpen={setIsOpenRemove}
+                    setSelectedProject={setSelectedProject}
+                />
+            );
+        });
+    };
+
+    const getRows = () => {
+        let rows = [];
+        const projects = getProjects();
+        for (let i = 0; i < projects.length; i += 2) {
+            rows.push(
+                <div key={i} className={styles.main__row}>
+                    {projects[i]}
+                    {projects[i + 1]}
+                </div>
+            );
+        }
+        return rows;
+    };
+
+    const deleteProject = async () => {
+        const dataForDB = {
+            companyID: companyID,
+            projectName: selectedProject,
+        };
+        try {
+            await fetch(`/api/deleteProject/`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(dataForDB),
+            });
+            setIsOpenRemove(false);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <>
+            <DeleteModal
+                title="Eliminar proyecto"
+                message="¿Deseas eliminar este proyecto?"
+                buttonText="Eliminar"
+                setIsOpen={setIsOpenRemove}
+                isOpen={isOpenRemove}
+                buttonAction={() => deleteProject()}
+            />
+            <NewProjectModal
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+                companyID={companyID}
+            />
+            <Sidebar selected={2} username="David Atias" />
+            <main className={styles.main}>
+                <div className={styles.main__header}>
+                    <Search placeholder="Buscar proyecto..." />
+                    <IconBox action={() => setIsOpen(true)}>
+                        <AddIcon fontSize="large" />
+                    </IconBox>
+                </div>
+                <div className={styles.main__content}>{getRows()}</div>
+            </main>
+        </>
+    );
+};
 
 export default Projects;
