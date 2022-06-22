44a9de2cb6b2dc2278c6ee4e2b7767d0a17f8114
David Atias <davidaar2000@gmail.com>
2022-06-22 09:43:39 -0600

    New deductions page with protected route and conditional render based on user role

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -7,15 +7,18 @@ export default async function middleware(req) {
 
 const handleRequest = (req, userData) => {
     const url = req.url;
-    if (url === 'http://localhost:3000/') {
+    if (url === "http://localhost:3000/") {
         return handleIndex(userData);
-    }
-    else if (url === 'http://localhost:3000/benefits') {
+    } else if (url === "http://localhost:3000/benefits") {
         return handleBenefits(userData);
     } else if (url === `http://localhost:3000/project`) {
         return handleProject(userData);
     } else if (url === `http://localhost:3000/employees`) {
         return handleEmployees(userData);
+    } else if (url === `http://localhost:3000/deductions`) {
+        return handleDeductions(userData);
+    } else if (url === `http://localhost:3000/hours`) {
+        return handleHours(userData);
     }
     else {
         return NextResponse.next();
@@ -26,7 +29,7 @@ const handleIndex = async (userData) => {
     if (userData) {
         return NextResponse.next();
     } else {
-        return NextResponse.redirect('http://localhost:3000/unauthorized');
+        return NextResponse.redirect("http://localhost:3000/unauthorized");
     }
 };
 
@@ -37,7 +40,7 @@ const handleBenefits = async (userData) => {
         response.headers.append("ids", JSON.stringify(ids));
         return response;
     } else {
-        return NextResponse.redirect('http://localhost:3000/unauthorized');
+        return NextResponse.redirect("http://localhost:3000/unauthorized");
     }
 };
 
@@ -69,12 +72,23 @@ const handleEmployees = async (userData) => {
     } else {
         return NextResponse.redirect(`http://localhost:3000/unauthorized`);
     }
-}
+};
+
+const handleDeductions = async (userData) => {
+    if (userData) {
+        const ids = await fetchIds(userData);
+        let response = NextResponse.next();
+        response.headers.append("ids", JSON.stringify(ids));
+        return response;
+    } else {
+        return NextResponse.redirect("http://localhost:3000/unauthorized");
+    }
+};
 
 const decodeToken = async (req) => {
     const { cookies } = req;
     const decoded = await (
-        await fetch('http://localhost:3000/api/services/validateToken', {
+        await fetch("http://localhost:3000/api/services/validateToken", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -91,7 +105,7 @@ const decodeToken = async (req) => {
 const fetchIds = async (userData) => {
     if (userData) {
         const ids = await (
-            await fetch('http://localhost:3000/api/services/fetchIds', {
+            await fetch("http://localhost:3000/api/services/fetchIds", {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
---
+++ pages/deductions.js
@@ -0,0 +1,95 @@
+import EmployerBenefits from "../components/EmployerDeductions";
+import EmployeeBenefits from "../components/EmployeeDeductions";
+import jwt from "jsonwebtoken";
+import { prisma } from "/.db";
+import safeJsonStringify from "safe-json-stringify";
+
+export async function getServerSideProps(context) {
+    const { req, res } = context;
+    const { cookies } = req;
+    console.log(res._headers.ids);
+    const ids = JSON.parse(res._headers.ids);
+    const employeeID = ids.id;
+    const companyID = ids.companyId;
+    const { userData } = jwt.verify(cookies.token, process.env.JWT_SECRET);
+    let isEmployer = userData.isEmployer;
+    if (isEmployer) {
+        let deductionsQuery = await prisma.deduccionVoluntaria.findMany({
+            where: {
+                cedulaJuridica: companyID,
+            },
+        });
+        let projectQuery = await prisma.proyecto.findMany({
+            where: {
+                cedulaJuridica: companyID,
+            },
+            select: {
+                nombre: true,
+                moneda: true,
+            },
+        });
+        const deductionString = JSON.parse(safeJsonStringify(deductionsQuery));
+        const proyectString = JSON.parse(safeJsonStringify(projectQuery));
+
+        return {
+            props: {
+                employerProps: {
+                    companyID,
+                    deductionString,
+                    proyectString,
+                    name: userData.name,
+                },
+                isEmployer,
+            },
+        };
+    } else {
+        let projects = await prisma.proyecto.findMany({
+            where: {
+                cedulaJuridica: companyID,
+            },
+            select: {
+                nombre: true,
+                moneda: true,
+            },
+        });
+        const hired = await prisma.esContratado.findMany({
+            where: {
+                cedulaJuridica: companyID,
+                cedulaEmpleado: employeeID,
+            },
+            select: {
+                nombreProyecto: true,
+                montoPago: true,
+            },
+        });
+
+        const hiredIn = JSON.parse(safeJsonStringify(hired));
+        const projectsString = JSON.parse(safeJsonStringify(projects));
+        return {
+            props: {
+                employeeProps: {
+                    employeeID,
+                    companyID,
+                    projectsString,
+                    hiredIn,
+                    employeeName: userData.name,
+                },
+                isEmployer,
+            },
+        };
+    }
+}
+
+const Benefits = (props) => {
+    return (
+        <>
+            {props.isEmployer ? (
+                <EmployerBenefits props={props.employerProps} />
+            ) : (
+                <EmployeeBenefits props={props.employeeProps} />
+            )}
+        </>
+    );
+};
+
+export default Benefits;
