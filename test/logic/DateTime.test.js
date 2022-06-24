16f177b7e92b56cb6bf46932eb76d604056d2e4f
David Atias <davidaar2000@gmail.com>
2022-06-23 22:05:36 -0600

    Added tests for business logic files

--- test/logic/DateTime.test.js
+++ test/logic/DateTime.test.js
@@ -42,3 +42,150 @@ describe("Date to String", () => {
             }).toThrowError("The separator is not a single character");
         });
 });
+
+describe("String to Date", () => {
+    it("should convert a string to a date", () => {
+        const date = new Date(2018, 0, 1);
+        expect(stringToDate("01-01-2018")).toEqual(date);
+    }),
+        it("should convert a string to a date with a custom separator", () => {
+            const date = new Date(2018, 0, 1);
+            expect(stringToDate("01/01/2018", "/")).toEqual(date);
+        }),
+        it("should throw an error if the dateString is not a string", () => {
+            expect(() => {
+                stringToDate(1);
+            }).toThrowError("The dateString is not a string");
+        }),
+        it("should throw an error if the dateString is not a valid date", () => {
+            expect(() => {
+                stringToDate("44-01-2018");
+            }).toThrowError("The dateString is not a valid date");
+        }),
+        it("should throw an error if the separator is not a string", () => {
+            expect(() => {
+                stringToDate("01-01-2018", 1);
+            }).toThrowError("The separator is not a string");
+        }),
+        it("should throw an error if the separator is length is 0", () => {
+            expect(() => {
+                stringToDate("01-01-2018", "");
+            }).toThrowError("The separator is empty");
+        }),
+        it("should throw an error if the separator is not a single character", () => {
+            expect(() => {
+                stringToDate("01-01-2018", "-/");
+            }).toThrowError("The separator is not a single character");
+        });
+});
+
+describe("Date to String with Time", () => {
+    it("should convert a date to a string", () => {
+        const date = new Date(2018, 0, 1);
+        expect(dateToStringWithTime(date)).toBe("01-01-2018 00:00");
+    }),
+        it("should convert a date to a string with a custom separator", () => {
+            const date = new Date(2018, 0, 1);
+            expect(dateToStringWithTime(date, "/")).toBe("01/01/2018 00:00");
+        }),
+        it("should throw an error if the date is not a date", () => {
+            expect(() => {
+                dateToStringWithTime(1);
+            }).toThrowError("The date is not a date");
+        }),
+        it("should throw an error if the separator is not a string", () => {
+            expect(() => {
+                dateToStringWithTime(new Date(), 1);
+            }).toThrowError("The separator is not a string");
+        }),
+        it("should throw an error if the separator is empty", () => {
+            expect(() => {
+                dateToStringWithTime(new Date(), "");
+            }).toThrowError("The separator is empty");
+        }),
+        it("should throw an error if the separator is not a single character", () => {
+            expect(() => {
+                dateToStringWithTime(new Date(), "-/");
+            }).toThrowError("The separator is not a single character");
+        });
+});
+
+describe("String to Date with Time", () => {
+    it("should convert a string to a date", () => {
+        const date = new Date(2018, 0, 1);
+        expect(stringToDateWithTime("01-01-2018 00:00")).toEqual(date);
+    }),
+        it("should convert a string to a date with a custom separator", () => {
+            const date = new Date(2018, 0, 1);
+            expect(stringToDateWithTime("01/01/2018 00:00", "/")).toEqual(date);
+        }),
+        it("should throw an error if the dateString is not a string", () => {
+            expect(() => {
+                stringToDateWithTime(1);
+            }).toThrowError("The dateString is not a string");
+        }),
+        it("should throw an error if the dateString is not a valid date", () => {
+            expect(() => {
+                stringToDateWithTime("44-01-2018 00:00");
+            }).toThrowError("The dateString is not a valid date");
+        }),
+        it("should throw an error if the separator is not a string", () => {
+            expect(() => {
+                stringToDateWithTime("01-01-2018 00:00", 1);
+            }).toThrowError("The separator is not a string");
+        }),
+        it("should throw an error if the separator is length is 0", () => {
+            expect(() => {
+                stringToDateWithTime("01-01-2018 00:00", "");
+            }).toThrowError("The separator is empty");
+        }),
+        it("should throw an error if the separator is not a single character", () => {
+            expect(() => {
+                stringToDateWithTime("01-01-2018 00:00", "-/");
+            }).toThrowError("The separator is not a single character");
+        });
+});
+
+describe("Add Days", () => {
+    it("should add days to a date", () => {
+        const date = new Date(2018, 0, 1);
+        expect(addDays(date, 1)).toEqual(new Date(2018, 0, 2));
+    }),
+        it("should add days to a date with a custom separator", () => {
+            const date = new Date(2018, 0, 1);
+            expect(addDays(date, 1, "/")).toEqual(new Date(2018, 0, 2));
+        }),
+        it("should throw an error if the date is not a date", () => {
+            expect(() => {
+                addDays(1, 1);
+            }).toThrowError("The date is not a date");
+        }),
+        it("should throw an error if the days is not a number", () => {
+            expect(() => {
+                addDays(new Date(), "1");
+            }).toThrowError("The days is not a number");
+        }),
+        it("should throw an error if the days is not an integer", () => {
+            expect(() => {
+                addDays(new Date(), 1.5);
+            }).toThrowError("The days is not an integer");
+        });
+});
+
+describe("Get Days difference", () => {
+    it("should get the days difference between two dates", () => {
+        const date1 = new Date(2018, 0, 2);
+        const date2 = new Date(2018, 0, 1);
+        expect(getDayDifference(date1, date2)).toBe(1);
+    }),
+        it("should throw an error if one of the dates is not a date", () => {
+            expect(() => {
+                getDayDifference(1, new Date());
+            }).toThrowError("One of the dates is not a date");
+        }),
+        it("should throw an error if one of the dates is an invalid date", () => {
+            expect(() => {
+                getDayDifference(new Date(), new Date("44-01-2018"));
+            }).toThrowError("One of the dates is an invalid date");
+        });
+});
---
+++ test/logic/Payroll.test.js
@@ -0,0 +1,244 @@
+import React from "react";
+import {
+    applyPercentageDeduction,
+    applyFixedDeduction,
+    calculateHourlySalary,
+    addBenefit,
+    getNextPaymentDate,
+    createPaymentField,
+    createPayment,
+} from "../../logic/Payroll";
+
+describe("Apply Percentage Deduction", () => {
+    it("should apply a percentage deduction to a salary", () => {
+        expect(applyPercentageDeduction(1000, 10)).toBe(900);
+    });
+    it("should throw an error if the salary is not a number", () => {
+        expect(() => applyPercentageDeduction("a", 10)).toThrow(
+            new Error("The salary is not a number")
+        );
+    });
+    it("should throw an error if the percentage is not a number", () => {
+        expect(() => applyPercentageDeduction(1000, "a")).toThrow(
+            new Error("The percentage deduction is not a number")
+        );
+    });
+    it("should throw an error if the salary is less than 0", () => {
+        expect(() => applyPercentageDeduction(-1, 10)).toThrow(
+            new Error("The salary is less than 0")
+        );
+    });
+    it("should throw an error if the percentage is less than 0", () => {
+        expect(() => applyPercentageDeduction(1000, -1)).toThrow(
+            new Error("The percentage deduction is less than 0")
+        );
+    });
+});
+
+describe("Apply Fixed Deduction", () => {
+    it("should apply a fixed deduction to a salary", () => {
+        expect(applyFixedDeduction(1000, 100)).toBe(900);
+    });
+    it("should throw an error if the salary is not a number", () => {
+        expect(() => applyFixedDeduction("a", 10)).toThrow(
+            new Error("The salary is not a number")
+        );
+    });
+    it("should throw an error if the fixed deduction is not a number", () => {
+        expect(() => applyFixedDeduction(1000, "a")).toThrow(
+            new Error("The fixed deduction is not a number")
+        );
+    });
+    it("should throw an error if the salary is less than 0", () => {
+        expect(() => applyFixedDeduction(-1, 10)).toThrow(
+            new Error("The salary is less than 0")
+        );
+    });
+    it("should throw an error if the fixed deduction is less than 0", () => {
+        expect(() => applyFixedDeduction(1000, -1)).toThrow(
+            new Error("The fixed deduction is less than 0")
+        );
+    });
+    it("should throw an error if the salary is less than the fixed deduction", () => {
+        expect(() => applyFixedDeduction(100, 1000)).toThrow(
+            new Error("The salary is less than the fixed deduction")
+        );
+    });
+});
+
+describe("Calculate Hourly Salary", () => {
+    it("should calculate the hourly salary", () => {
+        expect(calculateHourlySalary(200, 2500)).toBe(500000);
+    });
+    it("should throw an error if the hours are not a number", () => {
+        expect(() => calculateHourlySalary("a", 2500)).toThrow(
+            new Error("The hours are not a number")
+        );
+    });
+    it("should throw an error if the hourly rate is not a number", () => {
+        expect(() => calculateHourlySalary(200, "a")).toThrow(
+            new Error("The hourly rate is not a number")
+        );
+    });
+    it("should throw an error if the hours are less than 0", () => {
+        expect(() => calculateHourlySalary(-1, 2500)).toThrow(
+            new Error("The hours are less than 0")
+        );
+    });
+    it("should throw an error if the hourly rate is less than 0", () => {
+        expect(() => calculateHourlySalary(200, -1)).toThrow(
+            new Error("The hourly rate is less than 0")
+        );
+    });
+});
+
+describe("Add Benefit", () => {
+    it("should add a benefit to a salary", () => {
+        expect(addBenefit(1000, 100)).toBe(1100);
+    });
+    it("should throw an error if the salary is not a number", () => {
+        expect(() => addBenefit("a", 10)).toThrow(
+            new Error("The salary is not a number")
+        );
+    });
+    it("should throw an error if the benefit is not a number", () => {
+        expect(() => addBenefit(1000, "a")).toThrow(
+            new Error("The benefit is not a number")
+        );
+    });
+    it("should throw an error if the salary is less than 0", () => {
+        expect(() => addBenefit(-1, 10)).toThrow(
+            new Error("The salary is less than 0")
+        );
+    });
+    it("should throw an error if the benefit is less than 0", () => {
+        expect(() => addBenefit(1000, -1)).toThrow(
+            new Error("The benefit is less than 0")
+        );
+    });
+});
+
+describe("Get Next Payment Date", () => {
+    it("should get the next payment date", () => {
+        expect(getNextPaymentDate(new Date(2022, 0, 1), "Mensual")).toEqual(
+            new Date(2022, 0, 31)
+        );
+        expect(getNextPaymentDate(new Date(2022, 1, 1), "Mensual")).toEqual(
+            new Date(2022, 1, 28)
+        );
+        expect(getNextPaymentDate(new Date(2022, 0, 1), "Quincenal")).toEqual(
+            new Date(2022, 0, 15)
+        );
+        expect(getNextPaymentDate(new Date(2022, 1, 16), "Quincenal")).toEqual(
+            new Date(2022, 1, 28)
+        );
+        expect(getNextPaymentDate(new Date(2022, 0, 1), "Semanal")).toEqual(
+            new Date(2022, 0, 7)
+        );
+        expect(getNextPaymentDate(new Date(2022, 0, 7), "Semanal")).toEqual(
+            new Date(2022, 0, 7)
+        );
+        expect(getNextPaymentDate(new Date(2022, 0, 18), "Semanal")).toEqual(
+            new Date(2022, 0, 21)
+        );
+        expect(getNextPaymentDate(new Date(2022, 1, 28), "Semanal")).toEqual(
+            new Date(2022, 2, 4)
+        );
+    });
+
+    it("should throw an error if the payment date is not a date", () => {
+        expect(() => getNextPaymentDate("a", "Mensual")).toThrow(
+            new Error("The payment date is not a date")
+        );
+    });
+    it("should throw an error if the payment frequency is not a string", () => {
+        expect(() => getNextPaymentDate(new Date(2022, 1, 1), 1)).toThrow(
+            new Error("The frequency is not a string")
+        );
+    });
+    it("should throw an error if the frequency is not Mensual, Semanal, or Quincenal", () => {
+        expect(() =>
+            getNextPaymentDate(new Date(2022, 1, 1), "Diario")
+        ).toThrow(
+            new Error("The frequency is not Mensual, Quincenal or Semanal")
+        );
+    });
+    it("should throw an error if the payment date is an invalid date", () => {
+        expect(() =>
+            getNextPaymentDate(new Date("44-09-2022"), "Mensual")
+        ).toThrow(new Error("The payment date is an invalid date"));
+    });
+});
+
+describe("Create Payment Field", () => {
+    it("should create a payment field", () => {
+        expect(createPaymentField("Deduccion Pollolandia", 2000)).toEqual({
+            name: "Deduccion Pollolandia",
+            amount: 2000,
+        });
+        expect(createPaymentField("Beneficio 1", 202)).toEqual({
+            name: "Beneficio 1",
+            amount: 202,
+        });
+    });
+    it("should throw an error if the name is not a string", () => {
+        expect(() => createPaymentField(1, 2000)).toThrow(
+            new Error("The name is not a string")
+        );
+    });
+    it("should throw an error if the amount is not a number", () => {
+        expect(() => createPaymentField("Deduccion Pollolandia", "a")).toThrow(
+            new Error("The amount is not a number")
+        );
+    });
+    it("should throw an error if the amount is less than 0", () => {
+        expect(() => createPaymentField("Deduccion Pollolandia", -1)).toThrow(
+            new Error("The amount is less than 0")
+        );
+    });
+    it("should throw an error if the name is empty", () => {
+        expect(() => createPaymentField("", 2000)).toThrow(
+            new Error("The name is empty")
+        );
+    });
+});
+
+describe("Create Payment", () => {
+    it("should create a payment", () => {
+        expect(
+            createPayment(
+                100000,
+                [
+                    createPaymentField("Deduccion Obligatoria 1", 2000),
+                    createPaymentField("Deduccion Obligatoria 2", 250),
+                ],
+                [
+                    createPaymentField("Deduccion Voluntaria 1", 2000),
+                    createPaymentField("Deduccion Voluntaria 2", 250),
+                ],
+                [
+                    createPaymentField("Beneficio 1", 2000),
+                    createPaymentField("Beneficio 2", 250),
+                ]
+            )
+        ).toEqual({
+            salary: 100000,
+            benefits: [
+                { name: "Beneficio 1", amount: 2000 },
+                { name: "Beneficio 2", amount: 250 },
+            ],
+            totalBenefits: 2250,
+            voluntaryDeductions: [
+                { name: "Deduccion Voluntaria 1", amount: 2000 },
+                { name: "Deduccion Voluntaria 2", amount: 250 },
+            ],
+            totalVoluntaryDeductions: 2250,
+            mandatoryDeductions: [
+                { name: "Deduccion Obligatoria 1", amount: 2000 },
+                { name: "Deduccion Obligatoria 2", amount: 250 },
+            ],
+            totalMandatoryDeductions: 2250,
+            netSalary: 97750,
+        });
+    });
+});
