da20086f546f4c83e5a0e07d92249982b6118830
antipixel1 <75756211+antipixel1@users.noreply.github.com>
2022-07-13 21:46:20 -0600

    Notify employees if project is deleted

--- pages/api/deleteProject.js
+++ pages/api/deleteProject.js
@@ -1,4 +1,5 @@
 import {prisma} from '/.db';
+const { sendAlertDeletedProject } = require("/pages/api/services/mailServices");
 
 export default function handler (req, res) {
     if (req.method == "POST") {
@@ -10,6 +11,11 @@ async function deleteProject (req, res) {
     try {
         const { companyID, projectName } = req.body;
         const newName = 'Borrado: ' + projectName;
+        const employeesEmail = await getEmails(
+            res,
+            companyID,
+            projectName
+        );
         const updateProject = await prisma.proyecto.update({
             where: {
                 cedulaJuridica_nombre: {
@@ -21,9 +27,29 @@ async function deleteProject (req, res) {
                 habilitado: false,
             },
         });
+        for (let i = 0; i < employeesEmail.length; i++) {
+            await sendAlertDeletedProject(
+                employeesEmail[i].email,
+                projectName
+            );
+        }
         res.status(200).json(updateProject);
     } catch (error) {
         res.status(500);
         res.send(error.message);
     }
 };
+
+async function getEmails(res, companyID, projectName) {
+    try {
+        const employeesEmail = await prisma.$queryRaw`
+        SELECT email FROM hace_uso INNER JOIN escoge ON escoge.cedulaEmpleado = hace_uso.cedula
+        AND escoge.nombreProyecto = ${projectName}
+        AND escoge.cedulaJuridica = ${companyID};`;
+
+        return employeesEmail;
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+}
