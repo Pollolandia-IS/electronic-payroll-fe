b511f6979bcc27b50bcce97c9ea4054022c12d3b
antipixel1 <75756211+antipixel1@users.noreply.github.com>
2022-07-20 16:58:39 -0600

    Empty states aligned, also added project section empty state

--- pages/project.js
+++ pages/project.js
@@ -173,7 +173,13 @@ const Projects = ({ companyID, projectsString, name, isEmployer }) => {
                         <AddIcon fontSize="large" />
                     </IconBox>
                 </div>
-                { projectsString.length > 0 ? <div className={styles.main__content}>{getRows()}</div> : <div className={styles.main__noProjects}>Aún no hay proyectos <p style={{fontWeight: 400, fontSize: 20}}>Agrega tu primer proyecto en el botón +</p></div> }
+                { projectsString.length > 0 ? <div className={styles.main__content}>{getRows()}</div> : 
+                <>
+                <img className={styles.imgNoProjects} src="assets/img/404.png"></img> 
+                <div className={styles.noProjectsTitle}>Aún no hay proyectos 
+                <p style={styles.noProjects}>
+                    Agrega tu primer proyecto en el botón +</p></div> 
+                </>}
             </main>
         </>
     );
