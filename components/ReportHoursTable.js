60c95f96ed3ebcc7fb742b233eb2f71ce5103414
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-07-21 00:24:39 -0600

    Added functionality when reports are pending so that the buttons show in the view and work correctly

--- components/ReportHoursTable.js
+++ components/ReportHoursTable.js
@@ -1,4 +1,7 @@
 import DataTable from "./DataTable";
+import { IconButton, Tooltip } from "@mui/material";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import CancelOutlineIcon from "@mui/icons-material/CancelOutlined";
 
 export default function HourTable(props) {
     const columns = [
@@ -33,7 +36,51 @@ export default function HourTable(props) {
             align: "center",
             headerAlign: "center",
             disableColumnMenu: true,
-        }
+            renderCell: (params) => {
+                if (params.value === "Aprobado") {
+                    return "Aprobado";
+                } else if (params.value === "Pendiente") {
+                    return (
+                        <>
+                            <Tooltip title="Aprobar" arrow placement="top">
+                                <IconButton
+                                    size="small"
+                                    onClick={() => {
+                                        props.selectReport(
+                                            {
+                                                project: params.row.project,
+                                                date: params.row.date,
+                                            },
+                                            "approve"
+                                        );
+                                    }}
+                                >
+                                    <CheckCircleOutlineIcon color="success" />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Rechazar" arrow placement="top">
+                                <IconButton
+                                    size="small"
+                                    onClick={() => {
+                                        props.selectReport(
+                                            {
+                                                project: params.row.project,
+                                                date: params.row.date,
+                                            },
+                                            "reject"
+                                        );
+                                    }}
+                                >
+                                    <CancelOutlineIcon color="error" />
+                                </IconButton>
+                            </Tooltip>
+                        </>
+                    );
+                } else if (params.value === "Rechazado") {
+                    return "Rechazado";
+                }
+            },
+        },
     ];
 
     return (
