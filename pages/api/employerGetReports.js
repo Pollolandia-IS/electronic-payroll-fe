1149a77b09f903fb381c79926a2be7c4f26652f1
julian-solisaguilar <55472888+julian-solisaguilar@users.noreply.github.com>
2022-07-21 00:20:36 -0600

    Added needed querys for the view to update

--- pages/api/employerGetReports.js
+++ pages/api/employerGetReports.js
@@ -8,7 +8,7 @@ export default function handler(req, res) {
 
 async function getHoursEmployer(req, res) {
     try {
-        const {employerId, companyId} = req.body;
+        const { employerId, companyId } = req.body;
         const hours = await prisma.reporteHoras.findMany({
             where: {
                 cedulaEmpleador: employerId,
@@ -23,10 +23,51 @@ async function getHoursEmployer(req, res) {
             },
             orderBy: {
                 cedulaEmpleado: "asc",
-                fechaHora: "asc",
             },
         });
-        res.status(200).json(hours);
+
+        let counter = 0;
+        const hoursReportsWithId = hours.map((hourTime) => ({
+            id: counter++,
+            horasTrabajadas: hourTime.horasTrabajadas,
+            fechaHora: JSON.stringify(hourTime.fechaHora)
+                .split("T")[0]
+                .substring(1),
+            nombreProyecto: hourTime.nombreProyecto,
+            estado: hourTime.estado,
+            cedulaEmpleado: hourTime.cedulaEmpleado,
+        }));
+
+        let employeesId = [];
+        for (let i = 0; i < hours.length; i++) {
+            if (!employeesId.includes(hours[i].cedulaEmpleado)) {
+                employeesId.push(hours[i].cedulaEmpleado);
+            }
+        }
+
+        let employeesReports = [];
+        for (let i = 0; i < employeesId.length; i++) {
+            const employee = await prisma.persona.findUnique({
+                where: {
+                    cedula: employeesId[i],
+                },
+                select: {
+                    nombre: true,
+                    cedula: true,
+                },
+            });
+            employeesReports.push(`${employee.nombre},${employee.cedula}`);
+        }
+
+        JSON.stringify(hours);
+
+        const result = {
+            hoursReportsWithId: hoursReportsWithId,
+            hours: hours,
+            employeesReports: employeesReports,
+            employeesId: employeesId,
+        };
+        res.status(200).json(result);
     } catch (error) {
         res.status(500);
         res.send(error.message);
